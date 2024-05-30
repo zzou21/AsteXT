b75fa7bb3dd6df711d4cf8e2c124c67f113bae3f
Jerry Zou <zhihui.zou@duke.edu>
2024-05-29 22:37:26 -0400

    updated yearClassifier

--- yearClassifier/auxiliaryFilePreprocessing/cleantxtfixlines.py
+++ yearClassifier/auxiliaryFilePreprocessing/cleantxtfixlines.py
@@ -5,14 +5,12 @@ def convert_to_paragraph(input_file_path):
     
     # Strip newline characters and join words with a space
     paragraph = ' '.join(word.strip() for word in words)
-
     
     # Write the paragraph back to the file
     with open(input_file_path, "w", encoding="utf-8") as file:
         file.write(paragraph)
 
-# Example usage
-input_file_path = "/Users/Jerry/Desktop/Accomplice copy.txt"  # Path to your input text file
+input_file_path = "/Users/Jerry/Desktop/The Bridegroom by Ha Jin_processed.txt"  # Path to your input text file
 
 convert_to_paragraph(input_file_path)
 
---
+++ yearClassifier/auxiliaryFilePreprocessing/pdfconverterAndtextcleaner.py
@@ -0,0 +1,62 @@
+# This file is part of the auxiliary file preprocessing unit. It turns a story stored in PDF into a txt file
+import fitz  # PyMuPDF
+import os
+
+def pdfToText(pdfPath):
+    document = fitz.open(pdfPath)
+    text = ""
+    for page_num in range(document.page_count): # This 'for' loop iterates through each page of the PDF
+        page = document[page_num]
+        text += page.get_text("text")  # Extract text from the page
+    return text
+
+def cleanTxt(inputFileName, wordsToRemove):
+    # Read the content of the file
+    with open(inputFileName, "r", encoding="utf-8") as file:
+        content = file.read()
+    
+    for word in wordsToRemove:
+        content = content.replace(word, "")
+    
+    with open(inputFileName, "w", encoding="utf-8") as file:
+        file.write(content)
+
+def combineToParagraph(inputFile):
+    with open(inputFile, "r", encoding="utf-8") as file:
+        words = file.readlines()
+
+    #strip newline characters to join with a space
+    paragraph = ' '.join(word.strip() for word in words)
+    with open(inputFile, "w", encoding="utf-8") as file:
+        file.write(paragraph)
+
+if __name__ == "__main__":
+    # Path to your PDF file for pdfToText
+    inputPDFPath = "/Users/Jerry/Desktop/The Bridegroom by Ha Jin.pdf"
+    #Change the directory to where you wish the output txt file of the story you want to store
+    outputDirectoryPath = "/Users/Jerry/Desktop"
+
+    # File path to txt for cleanTxt
+    inputTxtPath = "/Users/Jerry/Desktop/Aftermath by Mary Yukari Waters_processed.txt"
+    #List of words to delete
+    wordsToRemove = ["the bridegroom"]
+
+    # File path to txt for combineToParagraph
+    inputCombineToParagraph = "/Users/Jerry/Desktop/The Bridegroom by Ha Jin_processed.txt"
+
+    storyName = os.path.basename(inputPDFPath).rsplit(".", 1)[0]
+    additionalFileName = "processed"
+    outputFileName = f"{storyName}_{additionalFileName}.txt"
+    outputFilePath = os.path.join(outputDirectoryPath, outputFileName)
+
+    # Comment out either "text = pdfToText(inputPDFPath)",  "cleanTxt(inputTxtPath, wordsToRemove)", or "combineToParagraph(inputCombineToParagraph)" depending on which one you need to use.
+
+    #text = pdfToText(inputPDFPath)
+    #with open(outputFilePath, "w", encoding="utf-8") as text_file:
+    #    text_file.write(text)
+    #print(f"PDF has been converted to plain text and saved as {outputFilePath}.")
+    
+    #cleanTxt(inputTxtPath, wordsToRemove)
+    #print(f"The words {wordsToRemove} have been removed from {inputTxtPath}.")
+
+    #combineToParagraph(inputCombineToParagraph)
